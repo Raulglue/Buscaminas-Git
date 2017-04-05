7a1df747212e7d8a94d1d395cc2abbdd2e55cebc
MrHugs <hsanchezs01ex@gmail.com>
2017-04-05 10:10:08 +0100

    Hola soy tu comit and push

--- src/vista/UIbusqui.java
+++ src/vista/UIbusqui.java
@@ -14,6 +14,12 @@ import java.awt.Component;
 import java.awt.Dimension;
 import java.awt.Rectangle;
 import java.awt.GridLayout;
+import javax.swing.JComboBox;
+import javax.swing.JList;
+import javax.swing.JMenuBar;
+import javax.swing.JMenu;
+import javax.swing.JTextField;
+import javax.swing.JMenuItem;
 
 public class UIbusqui extends JFrame {
 
@@ -21,14 +27,47 @@ public class UIbusqui extends JFrame {
 	private int tamaño=10;
 	protected JButton[][] botonera = new JButton[tamaño][tamaño];
 	protected JLabel lblBuscaminah;
+	private JMenuBar menuBar;
+	private JMenu mnJuego;
+	private JMenu mnPersonalizado;
+	private JLabel lblTamao;
+	private JTextField textField;
+	private JLabel label_1;
+	
 
 	/**
 	 * Create the frame.
 	 */
 	public UIbusqui() {
-		setMinimumSize(new Dimension(1300,1000));
+		setMinimumSize(new Dimension(500, 300));
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 300);
+		setBounds(100, 100, 583, 432);
+		
+		menuBar = new JMenuBar();
+		setJMenuBar(menuBar);
+		
+		mnJuego = new JMenu("Juego");
+		menuBar.add(mnJuego);
+		
+		mnPersonalizado = new JMenu("Personalizado");
+		mnJuego.add(mnPersonalizado);
+		
+		lblTamao = new JLabel("Tama\u00F1o");
+		lblTamao.setHorizontalAlignment(SwingConstants.CENTER);
+		mnPersonalizado.add(lblTamao);
+		
+		textField = new JTextField();
+		textField.setColumns(10);
+		mnPersonalizado.add(textField);
+		
+		label_1 = new JLabel("Minas");
+		label_1.setHorizontalAlignment(SwingConstants.RIGHT);
+		mnPersonalizado.add(label_1);
+		
+		JTextField txtMaxTamao = new JTextField();
+		txtMaxTamao.setText("MAX: tama\u00F1o\u00B2");
+		txtMaxTamao.setColumns(10);
+		mnPersonalizado.add(txtMaxTamao);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
@@ -39,11 +78,13 @@ public class UIbusqui extends JFrame {
 		lblBuscaminah.setBounds(new Rectangle(50, 0, 50, 0));
 		lblBuscaminah.setAlignmentX(Component.RIGHT_ALIGNMENT);
 		lblBuscaminah.setFont(new Font("Tahoma", Font.BOLD, 20));
-		contentPane.add(lblBuscaminah, BorderLayout.NORTH);
+		contentPane.add(lblBuscaminah, BorderLayout.SOUTH);
 		
 		JPanel panel = new JPanel();
 		contentPane.add(panel, BorderLayout.CENTER);
 		panel.setLayout(new GridLayout(0, botonera.length, 0, 0));
+		
+		
 		for (int i = 0; i < botonera.length; i++) {
 			for (int j = 0; j < botonera.length; j++) {
 				botonera[i][j] = new JButton();
