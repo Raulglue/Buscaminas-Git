82dc02fc15780d0613147321d9100fd75c07a42c
Raulglue <raul14-jl@hotmail.com>
2017-04-05 12:52:14 +0200

    prueba

---
+++ src/modelo/probando.java
@@ -0,0 +1,28 @@
+package modelo;
+
+public class probando {
+
+public static void main(String[] args) {
+	int matriz [][]= new int [5][5];
+	int contador=0;
+	for (int i = 0; i < matriz.length; i++) {
+		for (int j = 0; j < matriz.length; j++) {
+			matriz[i][j]=contador;
+			contador++;
+		}
+	}
+	int tamaño= matriz.length* matriz[0].length	;
+	int []array= new int[tamaño];
+	int j2=0;
+	for (int i = 0; i < array.length; i++) {
+		for (int j = 0; j < matriz.length; j++) {
+			array[i]= matriz[j][j2];
+		}
+		j2++;
+		
+	}
+	for (int i = 0; i < array.length; i++) {
+		System.out.println(array[i]);
+	}
+}
+}
